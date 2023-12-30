df77777f50bbb0818b159dc921ec953d803b5f6f
varun.nayak <varun.nayak@robosoftin.com>
2023-12-30 20:21:38 +0530

    testing user agents

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ import Type from './components/type/type'
 import Match from './components/match/match'
 
 function App() {
-  // const isWindows = navigator.userAgent.includes('Windows');
+  const isWindows = navigator.userAgent.includes('Windows');
 const isMac = navigator.userAgent.includes('Macintosh');
+const isIphone = navigator.userAgent.includes('iPhone');
 alert(navigator.userAgent);
  if (isMac) {
   alert('Mac');
-} else {
+} else if(isWindows){
   alert('Windows');
+}
+else if(isIphone){
+  alert('Iphone');
+}
+else {
+  alert('not found');
 }
   return (
     <>
