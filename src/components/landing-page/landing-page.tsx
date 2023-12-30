c74eef21406178efeadab9736b18010d839f28ce
varun.nayak <varun.nayak@robosoftin.com>
2023-12-30 21:46:04 +0530

    mobile type routing test

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./landing-page.scss"
+import { useNavigate } from 'react-router-dom'
 const LandingPage = () => {
+    const navigate = useNavigate()
+    const navigateDummy = () => {
+        navigate("/typeMobile");
+    }
   return (
     <div className='landing-page-container'>
-        <div className="bottom-circle"></div>
-        <div className="title-section">
+        <div className="bottom-circle"></div>()
+        <div className="title-section" onClick={navigateDummy}>
             KeyBlitz
         </div>
     </div>
